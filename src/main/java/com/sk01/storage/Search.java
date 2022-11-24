f3d1566029a7188f02f11e6669eb1e915e37b483
Marko Jovicic <38961465+marequest@users.noreply.github.com>
2022-11-24 16:20:15 +0100

    Converted File paths to Strings

--- src/main/java/com/sk01/storage/Search.java
+++ src/main/java/com/sk01/storage/Search.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public abstract class Search {
 
-    abstract public void getFiles(File dir) throws Exception;
+    abstract public void getFile(String path) throws Exception;
 
-    abstract public void getFiles(String podstring, String name) throws Exception;
+    abstract public void getFiles(String podstring) throws Exception;
 
-    abstract public void getFiles(String extension) throws Exception;
+    abstract public void getFilesWithExtension(String extension) throws Exception;
 
-    abstract public void getAllFiles(File dir) throws Exception;
+    abstract public void getAllFiles(String path) throws Exception;
 
-    abstract public boolean containsFiles(File dir, List fileNames) throws Exception;
+    abstract public boolean containsFiles(String path, List fileNames) throws Exception;
 
     abstract public File getDir(String fileName) throws Exception;
 
@@ -22,7 +22,7 @@ public abstract class Search {
 
     abstract public void sortByDate(List files) throws Exception;
 
-    abstract public void getFiles(File dir, Time pocetak, Time kraj) throws Exception;
+    abstract public void getFiles(String path, Time pocetak, Time kraj) throws Exception;
 
     abstract public void filtrate(String string) throws Exception;
 
